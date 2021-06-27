0f50d7b6cdd99d1cd53ab6bc7f4b8a832a8a741b
azharzulkifli98 <azharzulkifli98@gmail.com>
2021-06-26 21:48:01 -0500

    added functions to board class

--- cal.py
+++ cal.py
@@ -14,20 +14,65 @@ import random
 
 
 class Board:
-    
-    def __init__(self):
-        matrix = []
-        turn = 0
-        positions = []
+    # the matrix variable should not change after init
+    cols = 0
+    rows = 0
+    matrix = [[]]
+    turn = 0
+    positions = []
 
-    def get_board(self):
-        pass
+    def __init__(self, i, j, m, t, p):
+        """
+        example input looks like
+        g = Board([ [0, 0, 0, 0, 0], [1, 2, 1, -1, 0], 
+        [3, 0, 0, 2, 0], [0, 0, 0, 0, 1], [0, 0, 0, 0, 0] ], 
+        0, [ ['A', 5, 5], ['B', 5, 5], ['C', 5, 5] ])
+        """
+        self.rows = j
+        self.cols = i
+        self.matrix = m
+        self.turn = t
+        self.positions = p
 
-    def set_board(self):
-        pass
 
-    def move_piece(self):
-        pass
+    # positions will will determine whether to print
+    # tile number or piece on tile
+    def print_board(self):
+        for i in self.matrix:
+            for j in i:
+                occupy = False
+                for k in self.positions:
+                    if i in k and j in k:
+                        print(k[0])
+                        occupy = True
+                # only print number when no player is there
+                if not occupy:
+                    print(self.matrix[i][j])
+
+
+    # move piece according to position stored in positions
+    def update_piece(self, i, j, player, value):
+        for p in self.positions:
+            if p == [player, i, j]:
+                # TODO: fix this and test vals
+                newi = 
+                p = [player, newi, newj]
+
+
+
+    # checks value of tile and moves piece on it accordingly
+    def update_bonus_tile(self, i, j):
+        val = self.matrix[i][j]
+        self.update_piece(i, j, val)
+
+
+    def get_distance_from_goal(self, player):
+        for p in self.positions:
+            if p[0] == player:
+                # each row is worth num of cols
+                return (p[1] * self.rows) + p[2]
+
+
 
 
 class Game:
