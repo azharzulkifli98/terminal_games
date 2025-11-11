ba198041430adbb0955066c951460056ea596a62
azharzulkifli98 <azharzulkifli98@gmail.com>
2025-11-10 21:30:04 -0600

    made some improvements to birthday paradox

--- birthday_paradox.cs
+++ birthday_paradox.cs
@@ -7,7 +7,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-
 namespace Birthday
 {
     public class Tester
@@ -21,8 +20,6 @@ namespace Birthday
             for (;;) {
                 pick = random.Next(max_range);
                 if (found.Contains(pick)) {
-                    string total = string.Join(" ", found);
-                    Console.WriteLine(total);
                     return found.Count;
                 }
                 else {
@@ -31,21 +28,20 @@ namespace Birthday
             }
         }
 
-
-        public static int one_in_a_million(int max_range, int chosen) {
+        public static int one_in_a_million(int max_range) {
             int pick;
             int tries = 0;
+            int first = random.Next(max_range);
 
             for (;;) {
                 pick = random.Next(max_range);
                 tries++;
 
-                if (pick == chosen) {
+                if (pick == first) {
                     return tries;
                 }
             }
         }
-
 
         public static int deck_of_cards(int max_range, int chosen) {
             int pick;
@@ -70,13 +66,32 @@ namespace Birthday
             }
         }
 
-
         public static void Main(string[] args) {
-            Console.WriteLine("prob of repeat vs prob of deck of cards vs prob of blind guesses");
+            Console.WriteLine("Test the probability of getting any repeating number vs getting a specific repeat number");
+
+            int x = -1;
+            while (x < 1 || x > 1000)
+            {
+                Console.Write("Enter a positive integer between 1 and 1000: ");
+
+                string userInput = Console.ReadLine();
+
+                if (int.TryParse(userInput, out x))
+                {
+                    if (x > 1000 || x < 1)
+                    {
+                        Console.WriteLine("Integer out of bounds, try again.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Not an integer, try again.");   
+                }
+            }
             
-            Console.WriteLine(birthday_paradox(365));
-            Console.WriteLine(one_in_a_million(365, 100));
-            Console.WriteLine(deck_of_cards(365, 100));
+            Console.WriteLine("Any repeat: {0}", birthday_paradox(x));
+            Console.WriteLine("Specific repeat: {0}", one_in_a_million(x));
+            Console.WriteLine("Specific value with no repeats: {0}", deck_of_cards(x, 1));
         }
     }
 }
